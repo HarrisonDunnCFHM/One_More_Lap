8c83cfde1deef074a4e9d954625e2a103b407cbc
CFHM <hcdunn88@gmail.com>
2021-04-24 15:03:48 -0500

    Added music, start menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int sceneIndex;
+
+    private void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}
