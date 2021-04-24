2d3ea814d11662f6b7a9806216f4412ceaa3678e
CFHM <hcdunn88@gmail.com>
2021-04-23 21:39:33 -0500

    car is working - can turn and accel

---
+++ Assets/Scripts/Kart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Kart : MonoBehaviour
+{
+    //config params
+    [SerializeField] float maxTurn = 90f;
+    //[SerializeField] float turnSpeed = 2f;
+    [SerializeField] float maxSpeed = 10f;
+
+    //cache
+    Rigidbody2D myRigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerTurn();
+        PlayerSpeed();
+    }
+
+    private void PlayerTurn()
+    {
+        float controlDirection = CrossPlatformInputManager.GetAxis("Horizontal"); //value between -1 to +1
+        var kartRotation = controlDirection * -maxTurn;
+        myRigidBody.rotation = kartRotation;
+    }
+
+    private void PlayerSpeed()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical"); //value between -1 to +1
+        var kartSpeed = controlThrow * maxSpeed;
+        Debug.Log("Your speed is " + kartSpeed);
+        Vector2 kartVelocity = new Vector2(myRigidBody.velocity.x, kartSpeed);
+        myRigidBody.velocity = transform.up * kartSpeed;
+    }
+}
